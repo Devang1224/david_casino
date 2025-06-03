95b6f4378d7478bcad9fc9bcb0173185b3672b67
Devang1224 <97470914+Devang1224@users.noreply.github.com>
2025-06-03 09:24:49 +0530

    UI: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import Navbar from './components/Navbar'
-import GameGrid from './components/GameGrid'
-import ChatBox from './components/ChatBox'
+import Navbar from "./components/Navbar";
+import GameGrid from "./components/GameGrid";
+import ChatBox from "./components/ChatBox";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-appBlack">
       <Navbar />
       {/* <main className="flex-1 grid md:grid-cols-[3fr_1fr] gap-4 p-4">
         <GameGrid />
         <ChatBox />
       </main> */}
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="px-8 ">
+      <div className="py-4 flex justify-center">
+        <span className="px-[5px] py-[3px] text-white bg-[#F19117] rounded-sm ">
+          !
+        </span>
+        <span className="text-textGrey text-sm"> The legendary David Casino is back!</span>
+      </div>
+
+      <div className="bg-black py-3">
+          <div>
+
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
